75f312a7d087f25d09d93614746f22cc72f1cc61
Paul Bearne <pbearne@gmail.com>
2014-11-03 15:00:45 -0500

    first commit

---
+++ hello.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 1.6
+ */
+/*
+Plugin Name: Hello Dolly
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Matt Mullenweg
+Version: 1.6
+Author URI: http://ma.tt/
+*/
+
+/**
+ * @return string
+ */
+function hello_dolly_get_lyric() {
+	/** These are the lyrics to Hello Dolly */
+	$lyrics = _x( "Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+So, take her wrap, fellas
+Find her an empty lap, fellas
+Dolly'll never go away again
+Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+Golly, gee, fellas
+Find her a vacant knee, fellas
+Dolly'll never go away
+Dolly'll never go away
+Dolly'll never go away again", "lyrics: Hello, Dolly by Louis Armstrong" );
+
+	// Here we split it into lines
+	$lyrics = explode( "\n", $lyrics );
+
+	// And then randomly choose a line
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+ 
+/**
+ * This just echoes the chosen line, we'll position it later
+ */
+function hello_dolly() {
+	$chosen = hello_dolly_get_lyric();
+	printf( '<p id="dolly">$s</p>' , $chosen );
+}
+
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'hello_dolly' );
+
+
+/**
+ *  We need some CSS to position the paragraph
+ */
+function dolly_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
+	printf(' 
+	<style type="text/css">
+	#dolly {
+		float: %1$s;
+		padding-%1$s: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 11px;
+	}
+	</style>
+	', $x );
+}
+
+add_action( 'admin_head', 'dolly_css' );
+
+?>
---
+++ tests/test-hello.php
@@ -0,0 +1,70 @@
+<?php
+
+class HelloTest extends WP_UnitTestCase {
+
+	protected $lyrics;
+
+	/**
+	 * setup variables for tests
+	 */
+	public function setUp()
+	{
+		$this->lyrics[] = "Hello, Dolly";
+		$this->lyrics[] = "Well, hello, Dolly";
+		$this->lyrics[] = "It&#8217;s so nice to have you back where you belong";
+		$this->lyrics[] = "You&#8217;re lookin&#8217; swell, Dolly";
+		$this->lyrics[] = "I can tell, Dolly";
+		$this->lyrics[] = "You&#8217;re still glowin&#8217;, you&#8217;re still crowin&#8217;";
+		$this->lyrics[] = "You&#8217;re still goin&#8217; strong";
+		$this->lyrics[] = "We feel the room swayin&#8217;";
+		$this->lyrics[] = "While the band&#8217;s playin&#8217;";
+		$this->lyrics[] = "One of your old favourite songs from way back when";
+		$this->lyrics[] = "So, take her wrap, fellas";
+		$this->lyrics[] = "Find her an empty lap, fellas";
+		$this->lyrics[] = "Dolly&#8217;ll never go away again";
+		$this->lyrics[] = "Hello, Dolly";
+		$this->lyrics[] = "Well, hello, Dolly";
+		$this->lyrics[] = "It&#8217;s so nice to have you back where you belong";
+		$this->lyrics[] = "You&#8217;re lookin&#8217; swell, Dolly";
+		$this->lyrics[] = "I can tell, Dolly";
+		$this->lyrics[] = "You&#8217;re still glowin&#8217;, you&#8217;re still crowin&#8217;";
+		$this->lyrics[] = "You&#8217;re still goin&#8217; strong";
+		$this->lyrics[] = "We feel the room swayin&#8217;";
+		$this->lyrics[] = "While the band&#8217;s playin&#8217;";
+		$this->lyrics[] = "One of your old favourite songs from way back when";
+		$this->lyrics[] = "Golly, gee, fellas";
+		$this->lyrics[] = "Find her a vacant knee, fellas";
+		$this->lyrics[] = "Dolly&#8217;ll never go away";
+		$this->lyrics[] = "Dolly&#8217;ll never go away";
+		$this->lyrics[] = "Dolly&#8217;ll never go away again";
+	}
+
+
+	/**
+	 * makes sure a string is returned
+	 */
+	function test_is_string() {
+		// replace this with some actual testing code
+
+		$maybe_string = hello_dolly_get_lyric();
+		$this->assertInternalType( 'string', $maybe_string );
+	}
+
+	/**
+	 * make sure don't get an empty string
+	 */
+	function test_is_not_empty() {
+		// replace this with some actual testing code
+
+		$maybe_empty = hello_dolly_get_lyric();
+		$this->assertNotEmpty( trim( $maybe_empty ) );
+	}
+
+	/**
+	 * tests that the string we get matches the possible string in the array
+	 */
+	function test_is_in_array(){
+		$maybe_found = hello_dolly_get_lyric();
+		$this->assertContains( $maybe_found, $this->lyrics );
+	}
+}
